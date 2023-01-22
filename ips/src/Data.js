3c5aba48ddd6efd851d3a513c064f619e6cbd5a2
jivtesh singh <pahuls017@gmail.com>
2023-01-22 13:42:48 +0530

    changes done

---
+++ ips/src/Data.js
@@ -0,0 +1,62 @@
+const data = {
+     OfficeBearers : [
+        {"one": "Dr. Anirban Chatterjee","two" : "President"},
+        {"one": "Dr. Abhay Kolte","two" : "President Elect"},
+        {"one": "Dr. Nymphea Pandit","two" : "Imm. Past President"},
+        {"one": "Dr. Manish Khatri,","two" : "1st Vice President"},
+        {"one": "Dr. Baljeet Singh","two" : "2nd Vice President"},
+        {"one": "Dr. Harpreet Singh Grover","two" : "Secretary"},
+        {"one": "Dr. Vishakha Grover","two" : "Joint Secretary"},
+        {"one": "Dr. Neeraj Deshpande","two" : "Treasurer"},
+        {"one": "Dr. Ashish Kumar","two" : "Editor JISP"},
+        {"one": "Dr. Shipra Gupta","two" : "Scientific Program Convener"},
+        {"one": "Dr. Sanjeev Salaria","two" : "Periodontal Health Care Convener"},
+        {"one": "Dr Komal Puri","two" : "Assistant Secretary"},
+    ],
+    
+     Treasurers : [
+        {"one": "Dr. Sachin Goyal","two" : "Treasurer"},
+    ],
+    
+     Patrons : [
+        {"one": "Dr. Amarjit Singh Gill"},
+        {"one": "Dr. Vipin Bharti"},
+        {"one": "Dr. Poonam Sikri"},
+        {"one": "Dr. D.S. Kalsi"},
+        {"one": "Dr. Vikas Jindal"},
+    ],
+    
+     Advisors : [
+        {"one": "Dr. Arundeep Lamba","two" : "Dr. Ashu Bhardwaj"},
+        {"one": "Dr. Amit Bhardwaj","two" : "Dr. Bela Dave"},
+        {"one": "Dr. Gina Singh","two" : "Dr. Harinder Gupta"},
+        {"one": "Dr. Harjit Kaur","two" : "Dr. K.K. Chaubey"},
+        {"one": "Dr. K. K. Gupta","two" : "Dr. Kamalpreet Chhina"},
+        {"one": "Dr. R.K. Sharma","two" : "Dr. Ranjana Mohan"},
+        {"one": "Dr. K.S. Asi","two" : "Dr. M. Siddharth"},
+        {"one": "Dr. Nand Lal","two" : "Dr. Neeta Bhavsar"},
+        {"one": "Dr. Pardeep Shukla","two" : "Dr. Poonam Dutt"},
+        {"one": "Dr. N.D. Gupta","two" : "Dr. Ranjan Malhotra"},
+        {"one": "Dr. Sachit Anand","two" : "Dr. S.C. Narula"},
+        {"one": "Dr. Shikha Tiwari","two" : "Dr. Sohail Majid Jan"},
+        {"one": "Dr. (Brig)Subroto Roy","two" : "Dr. Vivek Govila"},
+    ],
+    
+     Coordinators : [
+        {"one": "Dr. Anshu Blaggana","two" : "Dr. A.K. Sharma"},
+        {"one": "Dr. Aaron Gomes","two" : "Dr. Amitabh Srivastava"},
+        {"one": "Dr. Anoop Kapoor","two" : "Dr. C.S. Baiju"},
+        {"one": "Dr. Farhan Durrani","two" : "Dr. Jitender"},
+        {"one": "Dr. Minnie Chadha","two" : "Dr. Nikhil Sharma"},
+        {"one": "Dr. Nitin Khuller","two" : "Dr. Neha Tak"},
+        {"one": "Dr. Pooja Palwankar","two" : "Dr. Rajesh Jhingran"},
+        {"one": "Dr. Rohit Jain","two" : "Dr. Roopa DA"},
+        {"one": "Dr. Sanjay Gupta","two" : "Dr(Col.) Satish Gupta"},
+        {"one": "Dr. Shweta Bali","two" : "Dr. Sumit Malhotra"},
+        {"one": "Dr. Suresh DK","two" : "Dr. Vandana"},
+        {"one": "Dr. Vikram Blaggana","two" : "Dr. Vivek Bains"},
+    ],
+}
+
+export default data;
+
